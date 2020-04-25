cf16e42f54336c1bc6ffda80ec7a70b11f548c5d
skraymond <skraymond+github@gmail.com>
2020-04-25 19:44:01 -0400

    Updating test

--- NER/test/test_vendors/test_southwest.py
+++ NER/test/test_vendors/test_southwest.py
@@ -8,8 +8,8 @@ class MyTestCase(unittest.TestCase):
     test_string = "test string"
     good_sw_text = "9/16/2019 Southwest Airlines | My Account, Trips, Past, Details\nPast " \
                    "Flight\n\nWashington (Reagan National), DC fo San Antonio, TX\nConfirmation # " \
-                   "NKQ6UG\n\nPASSENGER POINTS EARNED FARE TOTAL\n\nSPENCER RAYMOND + 1,708PTs $476.00\n\nRR " \
-                   "405696734\n\nROUTING DATE FARE TYPE FARE\n\nDCA to SAT 8/26/2019 Wanna Get Away " \
+                   "NKQ6UG\n\nPASSENGER POINTS EARNED FARE TOTAL\n\nFirstname Lastname + 1,708PTs $476.00\n\nRR " \
+                   "444112\n\nROUTING DATE FARE TYPE FARE\n\nDCA to SAT 8/26/2019 Wanna Get Away " \
                    "$142.33\nGov't taxes and fees $33 67\n\nTotal $176.00\n\nTotal points earned + 1," \
                    "708PTS\n\nhttps://www.southwest.com/myaccount/trips/past/details " \
                    "?confirmationNumber=NKQ6UG "
@@ -40,7 +40,7 @@ class MyTestCase(unittest.TestCase):
     def test_southwest_parse_error_01(self):
         parsed_text = "3/15/2019 Southwest Airlines | My Account, Trips, Past, Details\nPast Flight\n\nWashington (" \
                       "Reagan National), DC fo San Antonio, TX\nConfirmation # JYC6HX\n\nPASSENGER POINTS EARNED FARE " \
-                      "TOTAL\n\nSPENCER RAYMOND + 12,584PTs $586.68\n\nRR 405696734\n\nROUTING DATE FARE TYPE " \
+                      "TOTAL\n\nFirstname Lastname + 12,584PTs $586.68\n\nRR 444112\n\nROUTING DATE FARE TYPE " \
                       "FARE\n\nDCA to SAT 2/11/2019 Business Select $524.35\n\nGov't taxes and fees $62.33\n\nTotal " \
                       "°586.68\n\nTotal points earned + 12, " \
                       "584PTS\n\nhttps://www.southwest.com/myaccount/trips/past/details ?confirmationNumber=JY C6HX "
@@ -49,7 +49,7 @@ class MyTestCase(unittest.TestCase):
     def test_southwest_parse_error_02(self):
         parsed_text = "1/22/2019 Southwest Airlines | My Account, Trips, Past, Details\nPast Flight\n\nSan Antonio, " \
                       "TX to Washington (Reagan National), DC\nConfirmation # JWZM4P\n\nPASSENGER POINTS EARNED FARE " \
-                      "TOTAL\n\nSPENCER RAYMOND + 3,170PTS $310.90\n\nRR 405696734\n\nROUTING DATE FARE TYPE " \
+                      "TOTAL\n\nFirstname Lastname + 3,170PTS $310.90\n\nRR 444112\n\nROUTING DATE FARE TYPE " \
                       "FARE\n\nSAT toDCA 12/06/2018 Wanna Get Away $264.19\n\nGov't taxes and fees 546.71\n\nTotal " \
                       "°310.90\n\nTotal points earned + 3," \
                       "170PTS\n\nhttps://www.southwest.com/myaccount/trips/past/details ?confirmationNumber=JWZM4P "
@@ -57,8 +57,8 @@ class MyTestCase(unittest.TestCase):
 
     def test_southwest_parse_error_03(self):
         parsed_text = "4/16/2019 Southwest Airlines | My Account, Trips, Past, Details\n\nPast Flight\n\nApr 7\n\nLos " \
-                      "Angeles, CA to San Antonio, TX\nConfirmation # QHCO8O\n\nPASSENGER POINTS EARNED\n\nSPENCER " \
-                      "RAYMOND + 3,784?TS\n\nRR 405696734\n\nROUTING DATE\n\nLAX to SAT " \
+                      "Angeles, CA to San Antonio, TX\nConfirmation # QHCO8O\n\nPASSENGER POINTS EARNED\n\nFirstname " \
+                      "Lastname + 3,784?TS\n\nRR 444112\n\nROUTING DATE\n\nLAX to SAT " \
                       "4/07/2019\n\nhttps://www.southwest.com/myaccount/trips/past/details " \
                       "?confirmationNumber=QHCO8O\n\nFARE TYPE\n\nWanna Get Away\n\nGov't taxes and " \
                       "fees\n\nTotal\n\nTotal points earned\n\nFARE " \
@@ -68,7 +68,7 @@ class MyTestCase(unittest.TestCase):
     def test_southwest_parse_error_04(self):
         parsed_text = "4/16/2019 Southwest Airlines | My Account, Trips, Past, Pricing, Details\n\nPast Flight\n\nApr " \
                       "10\n\nSan Antonio, TX to Washington (Reagan National), DC\n\nConfirmation # " \
-                      "JXXYVR\n\nPASSENGER POINTS EARNED\n\nSPENCER RAYMOND + 12,584PTs\n\nRR 405696734\n\nROUTING " \
+                      "JXXYVR\n\nPASSENGER POINTS EARNED\n\nFirstname Lastname + 12,584PTs\n\nRR 444112\n\nROUTING " \
                       "DATE\n\nSAT toDCA 4/10/2019\n\nhttps://www.southwest.com/myaccount/trips/past/pricing/details " \
                       "?confirmationNumber=JXXYVR\n\nFARE TYPE\n\nBusiness Select\n\nGov't taxes and " \
                       "fees\n\nTotal\n\nTotal points earned\n\nFARE " \
@@ -78,7 +78,7 @@ class MyTestCase(unittest.TestCase):
     def test_southwest_parse_error_05(self):
         parsed_text = "3/15/2019 Southwest Airlines | My Account, Trips, Past, Details\n\nPast Flight\n\nMar 6\n\nSan " \
                       "Antonio, TX to Washington (Reagan National), DC\n\nConfirmation # WFLJ7G\n\nPASSENGER POINTS " \
-                      "EARNED\n\nSPENCER RAYMOND + 12,584PTs\n\nRR 405696734\n\nROUTING DATE\n\nSAT toDCA " \
+                      "EARNED\n\nFirstname Lastname + 12,584PTs\n\nRR 444112\n\nROUTING DATE\n\nSAT toDCA " \
                       "3/06/2019\n\nhttps://www.southwest.com/myaccount/trips/past/details " \
                       "?confirmationNumber=WFLJ7G\n\nFARE TYPE\n\nBusiness Select\n\nGov't taxes and " \
                       "fees\n\nTotal\n\nTotal points earned\n\nFARE " \
@@ -88,7 +88,7 @@ class MyTestCase(unittest.TestCase):
     def test_southwest_parse_error_06(self):
         parsed_text = "9/16/2019 Southwest Airlines | My Account, Trips, Past, Details\n\nPast Flight\n\nAug " \
                       "30\n\nSan Antonio, TX to Washington (Reagan National), DC\n\nConfirmation # " \
-                      "NKUKRW\n\nPASSENGER POINTS EARNED\nSPENCER RAYMOND + 3,080°TS\n\nRR 405696734\n\nROUTING " \
+                      "NKUKRW\n\nPASSENGER POINTS EARNED\nFirstname Lastname + 3,080°TS\n\nRR 444112\n\nROUTING " \
                       "DATE\n\nSAT toDCA 8/30/2019\n\nhttps://www.southwest.com/myaccount/trips/past/details" \
                       "?confirmationNumber=NKUKRW\n\nFARE TYPE\n\nWanna Get Away\n\nGov't taxes and " \
                       "fees\n\nTotal\n\nTotal points earned\n\nFARE " \
